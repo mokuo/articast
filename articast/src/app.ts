485cf083ac259e4206bdffaa475b817793263f63
mokuo <tennis10988.yk@gmail.com>
2024-01-15 18:24:04 +0900

    インデントを半角スペース2つに変更

--- articast/src/app.ts
+++ articast/src/app.ts
@@ -18,71 +18,71 @@ const tracer = new Tracer();
  */
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> => {
-    let response: APIGatewayProxyResult;
+  let response: APIGatewayProxyResult;
 
-    // Log the incoming event
-    logger.info("Lambda invocation event", { event });
+  // Log the incoming event
+  logger.info("Lambda invocation event", { event });
 
-    // Append awsRequestId to each log statement
-    logger.appendKeys({
-        awsRequestId: context.awsRequestId,
-    });
-    // Get facade segment created by AWS Lambda
-    const segment = tracer.getSegment();
+  // Append awsRequestId to each log statement
+  logger.appendKeys({
+    awsRequestId: context.awsRequestId,
+  });
+  // Get facade segment created by AWS Lambda
+  const segment = tracer.getSegment();
 
-    if (!segment) {
-        response = {
-            statusCode: 500,
-            body: "Failed to get segment",
-        };
-        return response;
-    }
-
-    // Create subsegment for the function & set it as active
-    const handlerSegment = segment.addNewSubsegment(`## ${process.env._HANDLER}`);
-    tracer.setSegment(handlerSegment);
-
-    // Annotate the subsegment with the cold start & serviceName
-    tracer.annotateColdStart();
-    tracer.addServiceNameAnnotation();
-
-    // Add annotation for the awsRequestId
-    tracer.putAnnotation("awsRequestId", context.awsRequestId);
-    // Capture cold start metrics
-    metrics.captureColdStartMetric();
-    // Create another subsegment & set it as active
-    const subsegment = handlerSegment.addNewSubsegment("### MySubSegment");
-    tracer.setSegment(subsegment);
-
-    try {
-        // hello world code
-        response = {
-            statusCode: 200,
-            body: JSON.stringify({
-                message: "hello world",
-            }),
-        };
-        logger.info(`Successful response from API enpoint: ${event.path}`, response.body);
-    } catch (err) {
-        // Error handling
-        response = {
-            statusCode: 500,
-            body: JSON.stringify({
-                message: "some error happened",
-            }),
-        };
-        tracer.addErrorAsMetadata(err as Error);
-        logger.error(`Error response from API enpoint: ${err}`, response.body);
-    } finally {
-        // Close subsegments (the AWS Lambda one is closed automatically)
-        subsegment.close(); // (### MySubSegment)
-        handlerSegment.close(); // (## index.handler)
-
-        // Set the facade segment as active again (the one created by AWS Lambda)
-        tracer.setSegment(segment);
-        // Publish all stored metrics
-        metrics.publishStoredMetrics();
-    }
-
+  if (!segment) {
+    response = {
+      statusCode: 500,
+      body: "Failed to get segment",
+    };
     return response;
+  }
+
+  // Create subsegment for the function & set it as active
+  const handlerSegment = segment.addNewSubsegment(`## ${process.env._HANDLER}`);
+  tracer.setSegment(handlerSegment);
+
+  // Annotate the subsegment with the cold start & serviceName
+  tracer.annotateColdStart();
+  tracer.addServiceNameAnnotation();
+
+  // Add annotation for the awsRequestId
+  tracer.putAnnotation("awsRequestId", context.awsRequestId);
+  // Capture cold start metrics
+  metrics.captureColdStartMetric();
+  // Create another subsegment & set it as active
+  const subsegment = handlerSegment.addNewSubsegment("### MySubSegment");
+  tracer.setSegment(subsegment);
+
+  try {
+    // hello world code
+    response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "hello world",
+      }),
+    };
+    logger.info(`Successful response from API enpoint: ${event.path}`, response.body);
+  } catch (err) {
+    // Error handling
+    response = {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "some error happened",
+      }),
+    };
+    tracer.addErrorAsMetadata(err as Error);
+    logger.error(`Error response from API enpoint: ${err}`, response.body);
+  } finally {
+    // Close subsegments (the AWS Lambda one is closed automatically)
+    subsegment.close(); // (### MySubSegment)
+    handlerSegment.close(); // (## index.handler)
+
+    // Set the facade segment as active again (the one created by AWS Lambda)
+    tracer.setSegment(segment);
+    // Publish all stored metrics
+    metrics.publishStoredMetrics();
+  }
+
+  return response;
 };
