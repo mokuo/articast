aa8005bd89d08202e54ad1252fe85a5de6346b1b
mokuo <tennis10988.yk@gmail.com>
2024-01-30 14:17:38 +0900

    ArticleCrawlerBuilder 実装

--- articast/src/domain/Article/ArticleCrawlerBuilder.ts
+++ articast/src/domain/Article/ArticleCrawlerBuilder.ts
@@ -6,9 +6,10 @@ export interface IArticleCrawler {
 }
 
 export default class ArticleCrawlerBuilder {
+  // NOTE: 新しいクローラーを実装したら、ここに追加する
   private crawlers: IArticleCrawler[] = [new PublickeyCrawler()];
 
-  build(blogFeedUrl: string): IArticleCrawler {
+  getCrawler(blogFeedUrl: string): IArticleCrawler {
     const foundCrawler = this.crawlers.find((crawler) => crawler.blogFeedUrl === blogFeedUrl);
     if (foundCrawler === undefined) {
       throw new Error(`crawler not found: ${blogFeedUrl}`);
